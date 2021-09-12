02ef7d69ee5dbd414572dbc8ae2c3a94a74955c5
Julian Todd <julian@goatchurch.org.uk>
2021-09-12 09:17:49 +0100

    convert for everton

--- servehttps.py
+++ servehttps.py
@@ -4,6 +4,9 @@ server_address = ('localhost', 4443)
 httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
 httpd.socket = ssl.wrap_socket(httpd.socket,
                                server_side=True,
-                               certfile='localhost.pem',
+                               certfile='cert.pem', keyfile="key.pem",
                                ssl_version=ssl.PROTOCOL_TLS)
 httpd.serve_forever()
+
+# keyphrase 1234
+# then ignore the invalid certificate in the webbrowser
